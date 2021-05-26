a30e2a69483115477cba92f355c93d5dc44d8cc6
nuttawwn <64520736+nuttawwn@users.noreply.github.com>
2021-05-26 13:49:24 +0700

    Add files via upload

---
+++ Excersice4_Nuttawut_W.py
@@ -0,0 +1,12 @@
+eng_score = 90
+business_score =89
+com_system_score = 100
+com_pro_score = 100
+
+print("------------------Your Score-----------------")
+print("Foundation English               :",eng_score,"score ")
+print("Genaral Business                 :",business_score,"score ")
+print("Introduction to Computer Systems :",com_system_score,"score")
+print("Computer Programming             :",com_pro_score,"score")
+
+print("TAOSWAGBOY")
