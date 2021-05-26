256981620ba47338b60d218845adffb07ffca71e
nuttawwn <64520736+nuttawwn@users.noreply.github.com>
2021-05-26 17:11:32 +0700

    Add files via upload

---
+++ Exercise5_1_Nuttawut_W.py
@@ -0,0 +1,10 @@
+print("โปรแกรมตำนวณเลข")
+num_1 = int(input("จำนวนที่ 1 : "))
+num_2 = int(input("จำนวนที่ 2 : "))
+
+print("ผลลัพธ์การคำนวณโปรแกรมทั้ง 4 แบบ\n")
+
+print(num_1,"+",num_2,"=",(num_1+num_2))
+print(num_1,"-",num_2,"=",(num_1-num_2))
+print(num_1,"*",num_2,"=",(num_1*num_2))
+print(num_1,"/",num_2,"=",int((num_1/num_2)))
